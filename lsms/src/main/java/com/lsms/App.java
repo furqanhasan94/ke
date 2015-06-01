461ad0e765f150d5d33e89f1ab97727f3459b270
furqanhasan94 <furqan.hasan94@gmail.com>
2015-06-02 00:54:49 +0500

    Project ready for orm

--- lsms/src/main/java/com/lsms/App.java
+++ lsms/src/main/java/com/lsms/App.java
@@ -1,13 +1,30 @@
 package com.lsms;
 
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.inject.Named;
+
 /**
  * Hello world!
  *
  */
+
+
+@ManagedBean
+@RequestScoped
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public void App(){}
+    
+    private String name ;
+    
+    public void setName(String n){
+        name = n ;
     }
+    
+    public String getName(){
+        return name;
+      }
 }
